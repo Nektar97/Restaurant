7937668dfa21ac5b7120e716ad83d581ea948f2c
leskal <alexakis1997@gmail.com>
2018-01-15 00:38:45 +0200

    Organize html files and images
    Also now site works with flask

--- app.py
+++ app.py
@@ -1,7 +1,37 @@
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 import os
-from app import app
 
-if __name__ == 'main':
-	host = os.environ.get('IP', '127.0.0.1')
-	port = int(os.environ.get('PORT',80))
-	app.run(host=host, port=port)
+app = Flask(__name__)
+
+db_path = os.path.join(os.path.dirname(__file__), 'database.db')
+db_uri = 'sqlite:///{}'.format(db_path)
+app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
+app.config['SECRET_KEY'] = 'thisisasecret'
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/base')
+def base():
+    return render_template('base.html')
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+@app.route('/reservation')
+def reservation():
+    return render_template('reservation.html')
+
+@app.route('/sign_in')
+def sign_in():
+    return render_template('sign_in.html')
+
+if __name__ == '__main__':
+	app.run(debug=True)
